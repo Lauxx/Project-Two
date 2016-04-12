a818ee6a05b176e8a5dbbdefe0ff1bee237510e4
Lauxx <lm.nichols@hotmail.com>
2016-04-12 10:49:15 -0600

    now able to generate a static user id

--- routes/postHug.js
+++ routes/postHug.js
@@ -6,25 +6,26 @@ var Comments = require('../models/comments');
 //get- get everything; get one by id, put(update by id), delete(by id), post(generates its own id)
 
 //localhost:8000/api/postHug
-router.route('/postHug')
+router.route('/hugs')
 //this route gets all hug posts
 	.get(function(req, res){
-		PostHug.find(function(err, hugs){
+		PostHug.find(function(err, hug){
 			if(err){
 				console.log(err)
 			} else {
-				res.json({ message: 'getting all the hugs' })
+				res.json(hug)
 			}
 		});
 	})
 //this route will allow you to post a hug
 	.post(function(req, res){
+		var user_id = req.user ? req.user._id : "570c291724367ecc202e5d61";
 		var hug = new PostHug();
 
 		hug.title = req.body.title;
 		hug.content = req.body.content;
 		hug.duration = req.body.duration;
-		// hug.user = req.user._id;
+		hug.user = req.user_id || "570c291724367ecc202e5d61";
 
 		hug.save(function(err, hug){
 			if(err){
