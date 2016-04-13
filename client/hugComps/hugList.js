2e3e4b05cf361c900bb610f2187bb8cc5a9a122b
jerecockrell <jere.cockrell@gmail.com>
2016-04-13 13:18:48 -0600

    added more hug comps

---
+++ client/hugComps/hugList.js
@@ -0,0 +1,41 @@
+//	HugApp
+//	   NavBar
+//	   UserAuth
+//	   UserApp
+//	   HomePage(Logo/Carousel/Quotes)
+//	   UserData
+//		 UserDisplayCard
+//	   UserFormData
+//	     UserUpdateForm
+//	   HugPostData
+//		 HugPostForm	
+//	   HugListData
+//		 HugList
+//		   HugCard
+//			 CommentList
+//			 CommentPostData
+//				CommentPostForm
+//		 Footer
+
+var React = require('react');
+
+var HugCard = require('./hugCard');
+
+function HugList(props){
+	var hugs = props.hugArray.map(function(item){
+		return <HugCard 
+							title = {item.title}
+							content = {item.content}
+							duration = {item.duration}
+							user = {item.user}
+							comments = {item.comments}
+							/>
+	})
+		return (
+			<div>
+				{hugs}
+			</div>
+			)
+};
+
+module.exports = HugList;
