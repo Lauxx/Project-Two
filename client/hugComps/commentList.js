528f03cc951c460e07dcf60b70ac9681d88254e4
jerecockrell <jere.cockrell@gmail.com>
2016-04-13 15:55:51 -0600

    fixed comment functionality

--- client/hugComps/commentList.js
+++ client/hugComps/commentList.js
@@ -21,11 +21,12 @@ var React = require('react');
 
 function CommentList(props){
 	var comments = props.comments.map(function(comm){
+		var user = comm.user && comm.user.local ? comm.user.local.username : 'no user';
 		return (
 			<div>
 				<p>{comm.body}</p>
 				<p>{comm.date}</p>
-				<p>{comm.user.local.username}</p>
+				<p>{user}</p>
 			</div>
 			)
 	})
