cd20dcf61ef0ab4a93c03630ceefb0a2a1884ac9
Lauxx <lm.nichols@hotmail.com>
2016-04-14 13:09:55 -0600

    refactored the code to render the user in ejs will work on converting later

--- client/index.js
+++ client/index.js
@@ -1,7 +1,4 @@
 //	HugApp
-//	   NavBar
-//	   UserAuth
-//	   UserApp
 //	   HomePage(Logo/Carousel/Quotes)
 //	   UserData
 //		 UserDisplayCard
@@ -22,61 +19,19 @@
 
 var React = require('react');
 var ReactDom = require('react-dom');
-var NavBar = require('./navbar');
 var Footer = require('./footer');
-var UserAuth = require('./userAuth');
 var Home = require('./home');
-var UserApp = require('./userApp');
 var HugListData = require('./hugComps/HugListData');
 import HugsMap from './mapComps/map'
 
 require('./stylesheets/main.scss');
 
 var HugApp = React.createClass({
-	getInitialState: function(){
-		return {
-			activeComponent: 'home'
-		}
-	},
-
-	setActiveComponent: function(componentName){
-		this.setState({
-			activeComponent: componentName
-		})
-	},
-
-	getActiveComponent: function(){
-		return this.state.activeComponent
-	},
-	
-	showWhichComponent: function(){
-		switch(this.state.activeComponent){
-			case 'home' : 
-				return <Home />
-				break;
-			case 'login' :
-				return <UserApp login={ true } setActiveComponent={ this.setActiveComponent } />
-				break;
-			case 'signUp' :
-				return <UserApp login={ false } setActiveComponent={ this.setActiveComponent } />
-				break;
-			default: 
-			return <Home /> 
-		}
-	},
-
-
   render: function() {
-  	console.log("hello from hugs map");
-  	console.log(HugsMap);
     return (
       <div>
-      	<UserAuth >
-      	<div>
-      		<NavBar setActiveComponent={ this.setActiveComponent } getActiveComponent={ this.getActiveComponent }/>
-      		{ this.showWhichComponent() }
-      	</div>	
-      	</UserAuth>
+      	<Home />
+      	
       	<HugListData />
       	<HugsMap />
         <Footer />
--- routes/user.js
+++ routes/user.js
@@ -4,37 +4,37 @@ var User = require('../models/user');
 // app/routes.js
 module.exports = function(app, passport) {
 
-    app.post('/signup', function(req, res, next) { 
-      passport.authenticate('local-signup', function(err, user, info) {
-        console.log('sign up post', info);
-        if (err) { return next(err); }
-        if (!user) { return res.status(404).json(info.message);}
-        req.logIn(user, function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+    //create new user in database, hands over to passport to do its encryption
+
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('/', {
+            message: req.flash('signupMessage')
         });
-      })(req, res, next);
     });
 
- 
-    app.post('/login', function(req, res, next) {
-      passport.authenticate('local-login', function(err, user, info) {
-        console.log('login post', info);
-        if (err) { return next(err); }
-        if (!user) { return res.status(404).json(info.message);}
-        req.logIn(user, function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');
-        });
-      })(req, res, next);
-    });
-    
+    app.post('/login', passport.authenticate('local-login', {
+       successRedirect: '/', // redirect to the secure profile section
+       failureRedirect: '/', // redirect back to the signup page if there is an error
+       failureFlash: true // allow flash messages
+   }));
 
-    app.get('/logout', function(req, res) {
-       req.logout();
-       res.redirect('/');
-    });
-    
+   app.get('/login', function(req, res) {
+       // render the page and pass in any flash data if it exists
+       res.render('/', {
+           message: req.flash('loginMessage')
+       });
+   });
+
+   app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/')
+   })
 };
 
 
