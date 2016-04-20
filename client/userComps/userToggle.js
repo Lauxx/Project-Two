095f94cbb78771b8016d274c378933178b056c96
jerecockrell <jere.cockrell@gmail.com>
2016-04-20 15:01:56 -0600

    made cursor switch to hand on hover over icons

--- client/userComps/userToggle.js
+++ client/userComps/userToggle.js
@@ -20,11 +20,11 @@ function UserToggle(props){
 		return (
 			<div className="icons">
 				<div className="container col-lg-1 col-lg-offset-4">
-					<a onClick={ props.toggleActiveComp.bind(null,'userDisplay') } ><img src="img/user.png" /></a>
+					<a onClick={ props.toggleActiveComp.bind(null,'userDisplay')} ><img src="img/user.png" /></a>
 						<div> Your User Display</div>
 				</div>
 				<div className="container col-lg-1">
-					<a onClick={ props.toggleActiveComp.bind(null,'userUpdate') } ><img src="img/tools.png"/></a>
+					<a onClick={ props.toggleActiveComp.bind(null,'userUpdate')} ><img src="img/tools.png"/></a>
 						<div>Update Your User Profile </div>
 				</div>
 				<div className="container col-lg-1">
