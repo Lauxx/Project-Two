7dc0dae27beb8aed8f28ab84056cedcafca2aea7
Lauxx <lm.nichols@hotmail.com>
2016-04-21 11:48:56 -0600

    fixing toggle

--- client/userComps/userToggle.js
+++ client/userComps/userToggle.js
@@ -23,19 +23,19 @@ function UserToggle(props){
 
 			<div className="icons">
 				<div className="container col-lg-1 col-lg-offset-4">
-					<a onClick={ props.toggleActiveComp.bind(null,'userDisplay')} className="userIcon" ><img src="img/user.png" />
+					<a onClick={ props.toggleActiveComp.bind(null,'userDisplay')} className="userIcon" ><img src="img/user.png" /></a>
 						<p>Your User Display</p>
-					</a>
+					
 				</div>
 				<div className="container col-lg-1">
-					<a onClick={ props.toggleActiveComp.bind(null,'userUpdate')} className="userIcon" ><img src="img/tools.png" />
+					<a onClick={ props.toggleActiveComp.bind(null,'userUpdate')} className="userIcon" ><img src="img/tools.png"/></a>
 						<p>Update Your User Profile</p>
-					</a>
+					
 				</div>
 				<div className="container col-lg-1">
-					<a onClick={ props.toggleActiveComp.bind(null,'hugPost')} className="userIcon" ><img src="img/heart.png" />
+					<a onClick={ props.toggleActiveComp.bind(null,'hugPost')} className="userIcon" ><img src="img/heart.png"/></a>
 						<p id="heartText">Post a Hug</p>
-					</a>						
+											
 				</div>
 			</div>
 			
