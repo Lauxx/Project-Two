3a1f18c033dc81738b5f0c800d71e513d0dabe1f
Lauxx <lm.nichols@hotmail.com>
2016-04-22 17:18:03 -0600

    update display card

--- client/userComps/userDisplayCard.js
+++ client/userComps/userDisplayCard.js
@@ -25,11 +25,11 @@ var UserDisplayCard = React.createClass({
 		return (
 
 			<div className="container avenir">
-				<div className="col-lg-12">
-					<img src={this.props.user.local.profileImage } className="img-thumbnail"  width="304" height="236"/>
+				<div className="col-lg-3">
+					<img src={this.props.user.local.profileImage } className="img-thumbnail img"  width="304" height="236"/>
 				</div>
 				<div>
-				<h3> Welcome {this.props.user.local.username}!</h3>
+					<h3> Welcome {this.props.user.local.username}!</h3>
 				</div>
 			</div>
 			
