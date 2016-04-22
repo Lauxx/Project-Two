24449e75b641a26623306619fdee920368951935
jerecockrell <jere.cockrell@gmail.com>
2016-04-22 17:04:10 -0600

    trying for heroku again again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var options = {
 server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };  
-var mongodbUri = process.env.MONGOLAB_URI || "mongodb://localhost/blogPosts";
+var mongodbUri = process.env.MONGOLAB_URI || "mongodb://localhost/hugApp";
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
 
