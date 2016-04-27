6e991cd3665dded364e8617c630101d5f76f3422
Lauxx <lm.nichols@hotmail.com>
2016-04-27 15:48:38 -0600

    fixing gremlins

--- client/hugComps/hugCard.js
+++ client/hugComps/hugCard.js
@@ -61,8 +61,10 @@ var HugCard = React.createClass({
 		document.body.style.overflow = 'hidden';
 	},
 
+
 	afterOpenModal: function(){
-		this.refs.subtitle.style.color = 'black'
+		console.log(this.refs, 'afterOpenModal here');
+		/*this.refs.subtitle.style.color = 'black'*/
 	},
 
 	closeModal: function(){
@@ -77,8 +79,7 @@ var HugCard = React.createClass({
 		var commentForm = this.state.activeUser ? <CommentPostData id={this.props.id} loadHugsFromServer={this.props.loadHugsFromServer} user={this.props.user}/> : null;
 		var loggedInUser = this.state.activeUser && this.state.activeUser.local ? this.state.activeUser._id : loggedInUser;
 		
-
-	if(this.props.user && this.props.user._id === loggedInUser ){
+		console.log(this.props.user, 'modal card here');
 
 		return (
 			<div>
@@ -111,39 +112,7 @@ var HugCard = React.createClass({
 				</Modal>
 			</div>
 			)
-		} else {
-			return (
-				<div>
-			<Modal
-				isOpen={this.state.modalIsOpen}
-          		onAfterOpen={this.afterOpenModal}
-          		onRequestClose={this.closeModal}
-          		style={customStyles} >
-          		
-          		<div className="col-lg-5">
-          			<img src={ userImage } className="img-thumbnail"  width="250" height="250" />
-          		</div>
-          		<div className="col-lg-7">	
-          			<a><span className="glyph glyphicon glyphicon-remove-circle userIcon" title="Close" onClick={this.closeModal}></span></a>	
-          			<h2 className='avenir underline'>{this.props.title}</h2>
-          			<p> From @{ user }</p>
-          			<p> { this.props.content }</p>
-          			<p> Date of hug: { this.props.dayOfHug } </p>
-          			<p> { this.props.duration } </p>
-          			<a className='avenir' href={'https://www.google.com/maps/dir/Current+Location/' + this.props.lat + ',' + this.props.lng }>Get Directions </a>
-          		</div>	
-          			{ commentForm } 
 
-							<CommentList 
-								comments={this.props.comments} 
-								loadHugsFromServer={this.props.loadHugsFromServer} 
-								activeUser={ this.state.activeUser }/>
-          		
-          			
-				</Modal>
-			</div>
-				)
-		}
 	},
 
 	deleteHugPost: function(){
@@ -178,33 +147,30 @@ var HugCard = React.createClass({
 		var userImage = this.props.user && this.props.user.local ? this.props.user.local.profileImage : null;
 
 		
-		 if(this.props.user && this.props.user._id === loggedInUser ){
+		 if(this.props.user && this.props.user._id === loggedInUser){
 			return (
-			
-
 				<div className="well hugCard col-lg-4 col-md-3 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 ">
 					<div className="card">
 						<div className="avenir">
-  								<div className="col-lg-5 col-md-5">
-  								
+  							<div className="col-lg-5 col-md-5">
   								<img src={ userImage } className="img-thumbnail card-img-top img"  width="200" height="200"/>
-    							</div>
-    							<div className="col-lg-7 col-md-7">
+    						</div>
+    						<div className="col-lg-7 col-md-7">
     							<a><span className="glyph glyphicon glyphicon-remove-circle userIcon" title="Delete Your Hug" onClick={ this.deleteHugPost.bind(null, this.props.id) }></span></a>
     							<h4 className="card-title underline">{this.props.title} <small> from </small> @{user} </h4>
     							<p className="card-text">{this.props.content}</p>
     							<p className="card-text"> When: {this.props.dayOfHug}</p>
     							<p className="card-text"><small className="text-muted">{this.props.duration}</small></p>
-    							<a className='avenir' href={'https://www.google.com/maps/dir/Current+Location/' + this.props.lat + ',' + this.props.lng }>Get Directions </a>  						
-  								<a onClick={ this.openModal.bind(null, this.props.id) } className="comment-glyph userIcon"><img src='img/comment.png'/></a>	 
+    							<a className='avenir' href={'https://www.google.com/maps/dir/Current+Location/' 
+    								+ this.props.lat + ',' + this.props.lng }> Get Directions </a>  						
+  								<a onClick={ this.openModal.bind(null, this.props.id) } className="comment-glyph userIcon">
+  									<img src='img/comment.png'/>
+  								</a>	 
 								{ this.modalDisplay() }
-								
-						
-  								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				
+				</div>	
 			)
 
 		} else if(!loggedInUser){
@@ -236,31 +202,27 @@ var HugCard = React.createClass({
 			
 		} else {
 			return (
-				
 				<div className="well hugCard col-lg-4 col-md-3 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 ">
 					<div className="card">
 						<div className="avenir">
-  								
-  								<div className="col-lg-5 col-md-5">
-  									<img src={ userImage } className="img-thumbnail card-img-top img userIcon"  width="200" height="200"/>
-    							</div>
-    							<div className="col-lg-7 col-md-7">
-
-    								<h4 className="card-title underline">{this.props.title} <small> from </small> @{user} </h4>
-    								<p className="card-text">{this.props.content}</p>
-    								<p className="card-text"> When: {this.props.dayOfHug}</p>
-    								<p className="card-text"><small className="text-muted">{this.props.duration}</small></p>  						
-  									<a onClick={ this.openModal.bind(null, this.props.id) } className="comment-glyph userIcon"><img src='img/comment.png'/></a>
-									<a className='avenir' href={'https://www.google.com/maps/dir/Current+Location/' + this.props.lat + ',' + this.props.lng }>Get Directions </a>
-									{ this.modalDisplay() }
-								
-						
-  								</div>
+  							<div className="col-lg-5 col-md-5">
+  								<img src={ userImage } className="img-thumbnail card-img-top img"  width="200" height="200"/>
+    						</div>
+    						<div className="col-lg-7 col-md-7">
+    							<h4 className="card-title underline">{this.props.title} <small> from </small> @{user} </h4>
+    							<p className="card-text">{this.props.content}</p>
+    							<p className="card-text"> When: {this.props.dayOfHug}</p>
+    							<p className="card-text"><small className="text-muted">{this.props.duration}</small></p>
+    							<a className='avenir' href={'https://www.google.com/maps/dir/Current+Location/' 
+    								+ this.props.lat + ',' + this.props.lng }> Get Directions </a>  						
+  								<a onClick={ this.openModal.bind(null, this.props.id) } className="comment-glyph userIcon">
+  									<img src='img/comment.png'/>
+  								</a>	 
+								{ this.modalDisplay() }
+							</div>
 						</div>
 					</div>
 				</div>
-			
-
 				)
 		}
 	}
